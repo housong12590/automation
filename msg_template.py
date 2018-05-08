b481b8e54bb30d74bbe1dd651e93f88382bb1e03
hous <304536797@qq.com>
2018-05-08 11:25:20 +0800

    Initial commit

---
+++ msg_template.py
@@ -0,0 +1,14 @@
+message = {
+    "msgtype": "markdown",
+    "markdown": {"title": "杭州天气",
+                 "text": "#### chaos构建通知  \n > 9度，@1825718XXXX 西北风1级，空气良89，相对温度73%\n\n > ![screenshot](http://i01.lw.aliimg.com/media/lALPBbCc1ZhJGIvNAkzNBLA_1200_588.png)\n  > ###### 10点20分发布 [天气](http://www.thinkpage.cn/) "
+                 },
+    "at": {
+        "atMobiles": [
+            "1825718XXXX"
+        ],
+        "isAtAll": False
+    }
+}
+
+msg = "#### chaos构建通知  \n > 9度，@1825718XXXX 西北风1级，空气良89，相对温度73%\n\n > ![screenshot](http://i01.lw.aliimg.com/media/lALPBbCc1ZhJGIvNAkzNBLA_1200_588.png)\n  > ###### 10点20分发布 [天气](http://www.thinkpage.cn/)"
---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='automation',  # 应用名
+    version='1.1',  # 版本号
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'automation=cil.automation:main',
+        ],
+    }, install_requires=['paramiko', 'requests']
+)
