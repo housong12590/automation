012ef4719afcab9785e010483229a05f520176a2
hous <304536797@qq.com>
2018-05-19 22:32:36 +0800

    123123

--- docker/cli/docker_build.py
+++ docker/cli/docker_build.py
@@ -2,13 +2,13 @@ from docker import config
 from .command import run_command
 
 
-
 def build_push():
     IMAGE = '{}/{}'.format(config.REGISTRY, config.IMAGE_NAME)
     commands = [
         'docker login {} -u {} -p {}'.format(config.REGISTRY, config.REGISTRY_USER, config.REGISTRY_PASSWORD),
         'docker build -t {0}:{1} {2}'.format(IMAGE, config.IMAGE_TAG, config.DOCKER_FILE),
         'docker tag {0}:{1} {0}'.format(IMAGE, config.IMAGE_TAG),
+        'docker push {0} {0}'.format(IMAGE),
         'docker push {0} {0}:{1}'.format(IMAGE, config.IMAGE_TAG),
         'docker rmi -f {0}:{1} {0}'.format(IMAGE, config.IMAGE_TAG)
     ]
